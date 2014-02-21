ce1c340a80afde9bb7469a620ee642fb07f4bc7b
Simone Cianni <simone.cianni@bticino.it>
2014-02-21 14:59:49 +0100

    Add file for generate scrit for testing
    Signed-off-by: Simone Cianni <simone.cianni@bticino.it>

---
+++ test.c
@@ -0,0 +1,99 @@
+
+/* Author - Simone Cianni */
+
+// to build:	g++ test.c -o test
+// to run :	./test
+// to run :	./test <n>
+
+//sudo ./dhtest -V -m  00:53:44:11:51:12 -h ciaoooo
+
+/*
+macchanger -l | grep Bticin
+0849 - 00:03:50 - Bticino Spa
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/**
+ * random_ether_addr - Generate software assigned random Ethernet address
+ * @mac: Pointer to a six-byte array containing the Ethernet address
+ *
+ * Generate a random Ethernet address (MAC) that is not multicast
+ * and has the local assigned bit set.
+ */
+#define RAND_LEN 3
+#define LEN 20
+int new_rand_mach (char *mac)
+{
+        int fd;
+	unsigned char data[RAND_LEN] = "";
+
+	//char mac[10]="";
+
+        fd = open ("/dev/urandom", O_RDONLY);
+        if (fd > 0)
+		read( fd, data, RAND_LEN );
+        close (fd);
+
+	sprintf(mac,"00:03:50:%02X:%02X:%02X", data[0], data[1], data[2]);
+	//printf("%s \n", Mac);
+
+        data [0] &= 0xfe;       /* clear multicast bit */
+        data [0] |= 0x02;       /* set local assignment bit (IEEE802) */
+
+        return 1;
+}
+
+/*
+  hostname labels may contain only the ASCII letters 'a' through 'z'
+ (in a case-insensitive manner), the digits '0' through '9', and the hyphen ('-')
+ Each label must be between 1 and 63 characters long.
+*/
+int new_hostname (char *hostn, int n)
+{
+	unsigned char data[] = "host-";
+	sprintf(hostn,"%s%d", data,n);
+        return 1;
+}
+
+/*
+* main function
+* usge: ./test num
+* it produce a script for testing
+*/
+int main(int argc, char *argv[])
+{
+        int i;
+	char mac[19]="";
+	char hostn[LEN]="";
+	FILE *fp1;
+	fp1 = fopen("input.sh", "w+");
+	fprintf(fp1,"#!/bin/bash\n\n");
+
+	if(argc <= 1 ) {
+		new_rand_mach( mac );
+		new_hostname( hostn, 0 );
+		/* printf("%s , %s\n", mac, hostn); */
+		fprintf(fp1,"sudo ./dhtest  -V -m  %s -h %s\n", mac, hostn);
+        }
+	else {
+		for(i = 0; i < atoi(argv[1]); i++){
+			new_rand_mach( mac );
+			new_hostname( hostn, i );
+			/* printf("%s , %s\n", mac, hostn); */
+			fprintf(fp1,"sudo ./dhtest  -V -m  %s -h %s\n", mac, hostn);
+		}
+	}
+
+	fclose(fp1);
+	printf("Script created. usage: ./input.sh \n");
+        return 1;
+}
+#if 0
+....
+#endif
