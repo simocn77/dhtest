b9cc0cb0ac96647cd4c9f1ecb70e388c72ce692c
Simone Cianni <simone.cianni@bticino.it>
2014-02-27 10:40:39 +0100

    Redirect stderron on stdout Add possibility to set host name
    Signed-off-by: Simone Cianni <simone.cianni@bticino.it>

--- test.c
+++ test.c
@@ -19,6 +19,8 @@ macchanger -l | grep Bticin
 #include <sys/types.h>
 #include <sys/stat.h>
 
+//#include <string.h>
+
 /**
  * random_ether_addr - Generate software assigned random Ethernet address
  * @mac: Pointer to a six-byte array containing the Ethernet address
@@ -55,10 +57,20 @@ int new_rand_mach (char *mac)
  (in a case-insensitive manner), the digits '0' through '9', and the hyphen ('-')
  Each label must be between 1 and 63 characters long.
 */
-int new_hostname (char *hostn, int n)
+int new_hostname (char *hostn, const char *basehost, int n)
 {
+	/*
 	unsigned char data[] = "host-";
 	sprintf(hostn,"%s%d", data,n);
+	*/
+
+	/* Append */
+	/*
+	sprintf(hostn + strlen(hostn),"%d", n);
+	*/
+
+	sprintf(hostn,"%s-%d", basehost,n);
+
         return 1;
 }
 
@@ -66,16 +78,31 @@ int new_hostname (char *hostn, int n)
 * main function
 * usge: ./test num
 * it produce a script for testing
+* argc 3
+* argv[0] ./test
+* argv[1] 1		how many
+* argv[2] host          base hostname
+* argv[3] (null)
+*
 */
 int main(int argc, char *argv[])
 {
         int i;
 	char mac[19]="";
-	char hostn[LEN]="";
+	char hostn[63]=""; /* Max 63 characters */
+	char basehost[LEN]="";
 	FILE *fp1;
 	fp1 = fopen("input.sh", "w+");
 	fprintf(fp1,"#!/bin/bash\n\n");
 
+	/* len of argument : argc */
+
+	if (argc > 2)
+		sprintf(basehost, "%s",argv[2]);
+	else
+		sprintf(basehost, "host");
+
+	/*
 	printf("argc %i \n", argc);
 	printf("argv[0] %s \n", argv[0]);
 	printf("argv[1] %s \n", argv[1]);
@@ -83,20 +110,21 @@ int main(int argc, char *argv[])
 	printf("argv[3] %s \n", argv[3]);
 	printf("argv[4] %s \n", argv[4]);
 	printf("argv[5] %s \n", argv[5]);
+	*/
 
 	if(argc <= 1 ) {
 		new_rand_mach( mac );
-		new_hostname( hostn, 0 );
+		new_hostname( hostn, basehost, 0 );
 		/* printf("%s , %s\n", mac, hostn); */
-		fprintf(fp1,"sudo ./dhtest  -V -m  %s -h %s 2>&1\n", mac, hostn);
+		fprintf(fp1,"sudo ./dhtest -m  %s -h %s 2>&1\n", mac, hostn);
 		fprintf(fp1,"killall dhtest\n");
         }
 	else {
 		for(i = 0; i < atoi(argv[1]); i++){
 			new_rand_mach( mac );
-			new_hostname( hostn, i );
+			new_hostname( hostn, basehost, i );
 			/* printf("%s , %s\n", mac, hostn); */
-			fprintf(fp1,"sudo ./dhtest  -V -m  %s -h %s 2>&1\n", mac, hostn);
+			fprintf(fp1,"sudo ./dhtest -m  %s -h %s 2>&1\n", mac, hostn);
 			fprintf(fp1,"killall dhtest\n");
 		}
 	}
